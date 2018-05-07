cc866efc40bc58d46b648f0729af0ab54458a154
Karen-he <helloKarenH@users.noreply.github.com>
2018-05-07 16:53:59 +1000

    fix bug

--- Harvester/swearing_label.py
+++ Harvester/swearing_label.py
@@ -1,16 +1,17 @@
 # couch db
-from couchdb import Server
+# from couchdb import Server
+import re
 
-server = Server('http://admin:sweetheart@115.146.86.47:5986/')
-try:
-    db = server['tweets_search_mel']
-except:
-    db = server.create('tweets_search_mel')
+# server = Server('http://admin:sweetheart@115.146.86.47:5986/')
+# try:
+#     db = server['tweets_search_mel']
+# except:
+#     db = server.create('tweets_search_mel')
 
 import csv
 with open('corpus.txt',encoding="utf8", errors='ignore') as inputfile:
     word_corpus=csv.reader(inputfile,delimiter='\n')
-    print(word_corpus)
+    # print(word_corpus)
     corpus=[]
     for row in word_corpus:
         corpus.append((', '.join(row)))
@@ -22,10 +23,10 @@ def lable_swearing(tweet):
         everyword=everyword.lower()
         everyword=re.sub('[^0-9a-zA-Z]+', '', everyword)
         if everyword in corpus:
-            print(everyword)
+            # print(everyword)
             count=count+1
     if count>0:
-        indexco+=1
+        # indexco+=1
         return 'Yes'
     else:
         return 'No'
